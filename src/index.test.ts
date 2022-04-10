ff5e156da922ec59448d76b2587dc74bc5ac9db1
Hiro Ari <hiro0107@users.noreply.github.com>
2022-04-10 20:39:27 +0900

    Add index as map parameter

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,19 +7,44 @@ const waitFor =
 describe("Promise serial", () => {
   it("should map array sequentially", async () => {
     const array = [5, 3, 1];
-    const result = await map(array, async (value) => {
+    const result = await map(array, async (value, i) => {
       await waitFor(value * 100);
-      return value;
+      return {v: value, i};
     });
-    expect(result).toEqual([5, 3, 1]);
+    expect(result).toEqual([
+      {v: 5, i: 0}, {v: 3, i: 1}, {v: 1, i: 2}
+    ]);
   })
   it("should map array with wrapper sequentially", async () => {
     const array = [5, 3, 1];
     const wrapper = wrap(array);
+    const result = await wrapper.map(async (value, i) => {
+      await waitFor(value * 100);
+      return {v: value, i};
+    });
+    expect(result).toEqual([
+      {v: 5, i: 0}, {v: 3, i: 1}, {v: 1, i: 2}
+    ]);
+  })
+  it("should map array sequentially (without index)", async () => {
+    const array = [5, 3, 1];
+    const result = await map(array, async (value) => {
+      await waitFor(value * 100);
+      return {v: value};
+    });
+    expect(result).toEqual([
+      {v: 5}, {v: 3}, {v: 1}
+    ]);
+  })
+  it("should map array with wrapper sequentially (without index)", async () => {
+    const array = [5, 3, 1];
+    const wrapper = wrap(array);
     const result = await wrapper.map(async (value) => {
       await waitFor(value * 100);
-      return value;
+      return {v: value};
     });
-    expect(result).toEqual([5, 3, 1]);
+    expect(result).toEqual([
+      {v: 5}, {v: 3}, {v: 1}
+    ]);
   })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-async function map<T, R>(array: T[], f: (value: T) => Promise<R>): Promise<R[]> {
+async function map<T, R>(array: T[], f: (value: T, index?: number) => Promise<R>): Promise<R[]> {
   const result: R[] = [];
-  for (const value of array) {
-    result.push(await f(value));
+  for (let i = 0; i < array.length; i++) {
+    const value = array[i];
+    result.push(await f(value, i));
   }
   return result;
 }
 
 type Wrapper<T> = {
-  map: <R>(f: (value: T) => Promise<R>) => Promise<R[]>
+  map: <R>(f: (value: T, index?: number) => Promise<R>) => Promise<R[]>
 }
 
 
 function wrap<T>(array: T[]) {
   return {
-    map: <R>(f: (value: T) => Promise<R>) => map(array, f)
+    map: <R>(f: (value: T, index?: number) => Promise<R>) => map(array, f)
   } as Wrapper<T>;
 }
 export { map, wrap };
